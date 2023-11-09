5f7b80e757c200ccc5df33c4e31e0c9b2cbc7191
MaxPredathor <max.gilli98@gmail.com>
2023-11-09 14:43:02 +0100

    AppHeader component completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,26 @@
 <template>
     <header>
-        header
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="py-4">
+                    <img src="/images/dc-logo.png" alt="DC logo">
+                </div>
+                <div>
+                    <ul class="d-flex justify-content-between">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEO</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </div>
+            </div>
+        </div>       
     </header>
 </template>
 
@@ -12,5 +32,25 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/_variables' as *;
+    header{
+        background-color: $background_header;
+    }
+    ul{
+        list-style: none;
+
+        li{
+            padding: 50px 10px;
+            font-weight: bold;
+            color: $background_main;
+;
+
+            &:hover{
+                color: $color_primary;
+                border-bottom: 5px solid $color_primary;
+                padding: 50px 10px 45px 10px;
+            }
+        }
+    }
 
 </style>
